d6c29a112fa6db10fc14ce877a283d8789e50dc9
Kentaro Hayashi <hayashi@clear-code.com>
2026-08-04 14:21:35 +0900

    Add helper script to build on netlify
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

---
+++ scripts/build.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#
+# build script on netlify
+# 
+
+set -ex
+
+function info()
+{
+    MESSAGE="$1"
+    echo -e "[\e[32;40mINFO\e[0m] ${MESSAGE}"
+}
+
+function install_plugins()
+{
+    info "Install npm packages"
+    # Install the exact versions recorded in package-lock.json.
+    # --ignore-scripts: no dependency requires install-time scripts
+    npm ci --ignore-scripts
+}
+
+if [ "$(git rev-parse --is-shallow-repository)" = "true" ]; then
+    git fetch --unshallow
+fi
+git fetch origin 0.12
+git fetch origin 1.0
+
+install_plugins
+
+info "Setup worktree"
+for d in _build/0.12 _build/1.0; do
+    if [ -d "$d" ]; then
+        git worktree remove --force $d
+        rm -rf $d
+    fi
+done
+mkdir -p _build/{0.12,1.0}
+mkdir -p _book/{0.12,1.0}
+
+git worktree add _build/0.12 origin/0.12
+git worktree add _build/1.0 origin/1.0
+
+cp -f book.json FOOTER.md _build/0.12
+cp -r styles _layouts _build/0.12/
+cp -f book.json _build/1.0
+# TODO: Remove it when honkit migration branch was merged
+if [ "${HONKIT_ENV}" = "dev" ]; then
+    cp FOOTER.md _build/1.0/
+    rsync -avz styles/ _build/1.0/styles/
+    rsync -avz _layouts/ _build/1.0/_layouts/
+fi
+
+info "Building 0.12 branch"
+(cd _build/0.12 && npx honkit build . ../../_book/0.12)
+info "Building 1.0 branch"
+(cd _build/1.0 && npx honkit build . ../../_book/1.0)
+info "Building latest branch"
+rsync -avz _book/1.0/ _book/
+info "Done"
