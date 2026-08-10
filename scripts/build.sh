a12683834756bf11a1bf4636822feb2de1a42ebd
Kentaro Hayashi <hayashi@clear-code.com>
2026-08-10 11:20:36 +0900

    1.0: Fix title for 0.12 documentation
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -42,6 +42,7 @@ git worktree add _build/0.12 origin/0.12
 git worktree add _build/1.0 origin/1.0
 
 cp -f book.json FOOTER.md _build/0.12
+sed -i -e 's/Fluentd 1.0/Fluentd 0.12/' _build/0.12/book.json
 cp -r styles _layouts _build/0.12/
 cp -f book.json _build/1.0
 
