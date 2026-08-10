98e7fabca65613da6cfa9a87de0b5d7f3f8eba6f
Kentaro Hayashi <kenhys@gmail.com>
2026-08-10 11:39:24 +0900

    Apply additional honkit migration (#672)
    * 1.0: Ignore working directory (_build)
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>
    
    * 1.0: add missing redirects for 0.12 and 1.0
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>
    
    * 1.0: drop needless fixup
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>
    
    * 1.0: Fix title for 0.12 documentation
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>
    
    ---------
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -42,14 +42,9 @@ git worktree add _build/0.12 origin/0.12
 git worktree add _build/1.0 origin/1.0
 
 cp -f book.json FOOTER.md _build/0.12
+sed -i -e 's/Fluentd 1.0/Fluentd 0.12/' _build/0.12/book.json
 cp -r styles _layouts _build/0.12/
 cp -f book.json _build/1.0
-# TODO: Remove it when honkit migration branch was merged
-if [ "${HONKIT_ENV}" = "dev" ]; then
-    cp FOOTER.md _build/1.0/
-    rsync -avz styles/ _build/1.0/styles/
-    rsync -avz _layouts/ _build/1.0/_layouts/
-fi
 
 info "Building latest branch"
 npx honkit build
